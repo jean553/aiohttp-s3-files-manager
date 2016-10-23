f391a6b5ad07e63ed2f68b1d00264417581f22c6
jean <jean@bst>
2016-10-23 21:05:09 +0800

    integrate streaming response and asynchronous s3 requests

--- aiohttp-s3-files-manager/__main__.py
+++ aiohttp-s3-files-manager/__main__.py
@@ -14,9 +14,22 @@ def main():
     """
 
     app = web.Application()
-    app.router.add_get('/ping', handle_get_ping)
-    app.router.add_get('/connectivity-status', handle_get_connectivity_status)
-    app.router.add_get('/file', handle_get_file)
+
+    app.router.add_get(
+        '/ping',
+        handle_get_ping,
+    )
+
+    app.router.add_get(
+        '/connectivity-status',
+        handle_get_connectivity_status,
+    )
+
+    app.router.add_get(
+        '/file',
+        handle_get_file
+    )
+
     web.run_app(app)
 
 if __name__ == "__main__":
--- aiohttp-s3-files-manager/handle_get_file.py
+++ aiohttp-s3-files-manager/handle_get_file.py
@@ -8,9 +8,9 @@ import aiobotocore
 from aiohttp import web
 
 @asyncio.coroutine
-def handle_get_file(loop):
+def handle_get_file(request):
 
-    session = aiobotocore.get_session(loop=loop)
+    session = aiobotocore.get_session()
 
     client = session.create_client(
         service_name="s3",
@@ -25,6 +25,25 @@ def handle_get_file(loop):
         Key="key",
     )
 
-    print(response)
+    stream = response["Body"]
 
-    return web.Response(text="OK")
+    result = web.StreamResponse()
+    result.content_type = "text/plain"
+
+    yield from result.prepare(request)
+
+    try:
+        line = yield from stream.readline()
+        result.write(line)
+
+        while len(line) > 0:
+            line = yield from stream.readline()
+            result.write(line)
+    finally:
+        stream.close()
+
+    yield from result.write_eof()
+
+    client.close()
+
+    return result
